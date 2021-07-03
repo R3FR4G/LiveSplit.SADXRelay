a9edcb1a1700c7302fc10e52b17483cc90dda8d7
Refrag <R3FR4GYT@gmail.com>
2021-07-04 01:00:46 +0200

    Packet testing stuff

--- LiveSplit.SADXRelayServer/Program.cs
+++ LiveSplit.SADXRelayServer/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using LiveSplit.SADXRelayPacketLib;
 
 namespace LiveSplit.SADXRelayServer
 {
@@ -14,6 +17,51 @@ namespace LiveSplit.SADXRelayServer
         
         static async void DoWork()
         {
+            //packet testing stuff
+            Packet timepacket = new Packet(new TimeSpan(0, 47, 51));
+            Packet connectpacket = new Packet("AYT8");
+            Packet disconnectpacket = new Packet();
+            Packet responsepacket = new Packet(ResponseCode.BadId);
+
+            byte[] tp = timepacket.ToBytes();
+            byte[] cp = connectpacket.ToBytes();
+            byte[] dp = disconnectpacket.ToBytes();
+            byte[] rp = responsepacket.ToBytes();
+
+            List<byte[]> packets = new List<byte[]>();
+            packets.Add(tp);
+            packets.Add(cp);
+            packets.Add(dp);
+            packets.Add(rp);
+
+            List<string> strings = new List<string>();
+            
+            foreach (var packet in packets)
+            {
+                string temp = "";
+                foreach (var curbyte in packet)
+                {
+                    temp += curbyte.ToString() + " ";
+                }
+                strings.Add(temp);
+            }
+
+            foreach (var VARIABLE in strings)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+
+            Packet tpr = Packet.FromBytes(ref tp);
+            Packet cpr = Packet.FromBytes(ref cp);
+            Packet dpr = Packet.FromBytes(ref dp);
+            Packet rpr = Packet.FromBytes(ref rp);
+
+            Console.WriteLine(tpr.Type + ", " + tpr.Time);
+            Console.WriteLine(cpr.Type + ", " + cpr.Id);
+            Console.WriteLine(dpr.Type);
+            Console.WriteLine(rpr.Type + ", " + rpr.Response);
+
+            /*
             using (var udpClient = new UdpClient(3456))
             {
                 string response = "";
@@ -30,6 +78,7 @@ namespace LiveSplit.SADXRelayServer
                     }
                 }
             }
+            */
         }
     }
 }
