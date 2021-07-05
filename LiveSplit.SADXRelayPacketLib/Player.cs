229df1a35e1f65cb7b050bc69cb0e5cb6b904c0a
Refrag <R3FR4GYT@gmail.com>
2021-07-06 01:21:55 +0200

    Class for convenience on the server and probably receiver side

---
+++ LiveSplit.SADXRelayPacketLib/Player.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace LiveSplit.SADXRelayPacketLib
+{
+    public enum RelayTeam : byte
+    {
+        Europe,
+        America
+    }
+    
+    public class Player
+    {
+        public bool IsAuthenticated { get; set; } = false;
+        public string Id { get; }
+        public string Name { get; }
+        public RelayTeam Team { get; }
+
+        public Player(string id, string name, RelayTeam team)
+        {
+            Id = id;
+            Name = name;
+            Team = team;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Team: {Team.ToString()}, Authenticated: {IsAuthenticated}";
+        }
+    }
+}
